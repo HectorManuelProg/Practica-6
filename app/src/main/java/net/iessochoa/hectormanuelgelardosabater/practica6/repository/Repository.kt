c1b6d1c89213c02e811049a59f7d69da44a13721
Hector <hectormanuel.gs@hotmail.com>
2024-02-11 13:06:52 +0100

    ViewModel

---
+++ app/src/main/java/net/iessochoa/hectormanuelgelardosabater/practica6/repository/Repository.kt
@@ -0,0 +1,20 @@
+package net.iessochoa.hectormanuelgelardosabater.practica6.repository
+
+import android.app.Application
+import android.content.Context
+import net.iessochoa.hectormanuelgelardosabater.practica6.network.NetworkService
+
+object Repository {
+    //necesitaremos el context en las capas inferiores
+    private lateinit var application: Application
+    //iniciamos la instancia
+    operator fun invoke(context: Context) {
+        this.application = context.applicationContext as
+                Application
+        NetworkService(application)
+    }
+    suspend fun
+            getNextPersonajes()=NetworkService.getNextPersonajes()
+    fun getLiveDataListaPersonajes
+                ()=NetworkService.getLiveDataListaPersonajes()
+}
